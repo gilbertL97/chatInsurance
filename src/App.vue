b5528e2cc5a23b7e51e2735f984a6ce295d9bba2
Gilbert Luis <gilbertluis97@gmail.com>
2024-09-14 04:30:18 -0400

    fix some problemesn with primevue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import HeaderNav from './components/nav/HeaderNav.vue';
+
+</script>
+
+<template class="bg-white dark:bg-black text-warmgray">
+  <HeaderNav />
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/nav/HeaderNav.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { useDark, useToggle } from '@vueuse/core';
+import ToggleButton from 'primevue/togglebutton';
+import { ref, watch } from 'vue';
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+const checked = ref(isDark.value)
+
+watch(isDark, (newValue) => {
+    checked.value = newValue
+})
+
+const handleToggle = () => {
+    toggleDark()
+    checked.value = isDark.value
+}
+</script>
+
+<template>
+    <nav class="flex flex-row gap-2 items-center justify-center dark:text-fuchsia">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <ToggleButton v-model="checked" onLabel="On" offLabel="Off" @change="handleToggle" />
+    </nav>
+</template>
