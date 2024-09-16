100dcabdfb60f04561f63731291842bce26df732
Gilbert Luis <gilbertluis97@gmail.com>
2024-09-16 16:42:36 -0400

    import icons

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import '@/assets/main.css'
+import 'primeicons/primeicons.css'
+
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+
 
 const app = createApp(App)
 
