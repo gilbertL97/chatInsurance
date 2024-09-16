09fbde03ab6fe746bee547ed0db479b1157b5a3b
Gilbert Luis <gilbertluis97@gmail.com>
2024-09-16 16:40:34 -0400

    make flex vue main layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import HeaderNav from './components/nav/HeaderNav.vue';
-
+import 'primeicons/primeicons.css'
 </script>
 
-<template class="bg-white dark:bg-black text-warmgray">
-  <HeaderNav />
+<template>
+  <div
+    class="dark:bg-gray-800 bg-gray-100 text-gray-700 dark:text-gray-100 h-[100vh] flex flex-col items-center justify-center ">
+    <HeaderNav />
 
-  <RouterView />
+    <RouterView class="flex-grow" />
+
+  </div>
 </template>
 
 <style scoped></style>
