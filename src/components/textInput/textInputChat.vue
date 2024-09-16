c5477805b9e9e1c6b73023414689fe7cd4cd7a8d
Gilbert Luis <gilbertluis97@gmail.com>
2024-09-16 16:40:47 -0400

    starting chat page

---
+++ src/components/textInput/textInputChat.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext';
+import { ref } from 'vue';
+const text = ref('')
+
+</script>
+<template>
+    <div>
+        <InputText type="text" v-model="text" />
+    </div>
+</template>
+<style scoped></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,30 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+import ScrollPanel from 'primevue/scrollpanel';
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
+import { ref } from 'vue';
+const isPdfChat = ref(false);
+const view = () => {
+  isPdfChat.value = !isPdfChat.value
+}
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="w-full h-full overflow-hidden">
+    <Button @click="view()" label="teste" />
+    <Splitter unstyled class="mx-16 h-full">
+
+      <SplitterPanel class="flex items-center justify-center bg-blue-300 max-w-3/4" :minSize="75">
+        <ScrollPanel style="width: 100%; height: 200px">CHAT
+        </ScrollPanel>
+
+      </SplitterPanel>
+      <SplitterPanel v-if="isPdfChat" class="flex items-center justify-center bg-blue-800 w-1/4" :minSize="25">
+        Panel 2
+      </SplitterPanel>
+    </Splitter>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
