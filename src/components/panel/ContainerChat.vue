61973df9498608c4bbc263134231254f773fdae5
Gilbert Luis <gilbertluis97@gmail.com>
2024-09-17 16:12:20 -0400

    changin style and contain in panel chat

---
+++ src/components/panel/ContainerChat.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Textarea from 'primevue/textarea';
+import MessageChat from '../chat/messageChat.vue';
+
+defineProps<{ loading: boolean }>()
+
+</script>
+<template>
+    <div class="w-2/4 mb-auto ml-2 my-3 pl-3 rounded-lg border-slate-700 border-2">
+        <MessageChat :text="'testesv'" :is-question="true" :message="{ role: 'user', content: 'test' }"
+            :is-loading="loading" />
+    </div>
+    <div>
+
+        <Textarea v-model="value" rows="2" cols="30" />
+    </div>
+</template>
+<style scoped></style>
