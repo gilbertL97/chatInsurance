576218550f0553d933d156aa5d19ea798805661d
Gilbert Luis <gilbertluis97@gmail.com>
2024-09-17 12:48:46 -0400

    basic stling components message and panelpreview

---
+++ src/components/panel/panelPreviewPDF.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SplitterPanel from 'primevue/splitterpanel';
+
+defineProps<{ isPdfChat: boolean }>()
+</script>
+<template>
+    <SplitterPanel v-if="isPdfChat" class="flex items-center justify-center bg-blue-800 w-1/4" :minSize="25">
+        Panel 2
+    </SplitterPanel>
+</template>
+<style scoped></style>
