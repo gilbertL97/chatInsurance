5081b09a0e8e0fb00099f98188c77d9c4b4725ec
Gilbert Luis <gilbertluis97@gmail.com>
2024-09-17 16:11:29 -0400

    adding md render

---
+++ src/components/chat/messageRender.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MarkdownIt from "markdown-it";
+
+
+
+const props = defineProps<{
+    source: string;
+}>();
+
+const markdown = new MarkdownIt();
+</script>
+
+<template>
+    <div class="box-border min-w-52 ml-1" v-html="markdown.render(props.source)" />
+</template>
+
+<style scoped></style>
